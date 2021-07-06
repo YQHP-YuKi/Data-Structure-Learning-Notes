2028b5a176dd4d6cd2ded0d3109395a88d73acd9
YQHP-YuKi <1465722762@qq.com>
2021-07-06 16:50:41 +0800

    This commit is in the schoole UF_Tree

--- src/uf/UF_Tree.java
+++ src/uf/UF_Tree.java
@@ -6,4 +6,60 @@ package uf;
  */
 public class UF_Tree {
 
+  /**
+   * 记录节点元素和该元素所在分组的标识
+   */
+  private int[] eleAndGroup;
+  /**
+   * 记录并查集中数据的分组个数
+   */
+  private int count;
+
+  /**
+   * 初始化并查集
+   */
+  public UF_Tree(int N) {
+    //初始化分组的数量,默认情况下有N个分组
+    this.count = N;
+    //初始化eleAndGroup数组
+    this.eleAndGroup = new int[N];
+    //初始化eleAndGroup中的元素及其所在的组的标识符,让eleAndGroup数组的索引作为并查集的每个节点的元素
+    for (int i = 0; i < eleAndGroup.length; i++) {
+      eleAndGroup[i] = i;
+    }
+  }
+
+  /**
+   * 获取当前并查集中的数据有多少个分组
+   */
+  public int count() {
+    return count;
+  }
+
+  /**
+   * 判断并查集中元素p和元素q是否在同一分组中
+   */
+  public boolean connected(int p, int q) {
+    return find(p) == find(q);
+  }
+
+  /**
+   * 元素p所在分组的标识符
+   */
+  public int find(int p) {
+    while (true) {
+      if (eleAndGroup[p] == p) {
+        return p;
+      } else {
+        p = eleAndGroup[p];
+      }
+    }
+  }
+
+  /**
+   * 把p元素所在分组和q元素所在分组合并
+   */
+  public void union(int p, int q) {
+
+  }
 }
